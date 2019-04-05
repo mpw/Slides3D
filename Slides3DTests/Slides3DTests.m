0e95b7336a2ca654f8476eed3eb9eac47d4cf5f7
Marcel Weiher <marcel@metaobject.com>
2019-04-05 11:20:36 +0200

    initial commit, already factored into framework

---
+++ Slides3DTests/Slides3DTests.m
@@ -0,0 +1,34 @@
+//
+//  Slides3DTests.m
+//  Slides3DTests
+//
+//  Created by Marcel Weiher on 4/13/14.
+//  Copyright (c) 2014 Apple Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Slides3DTests : XCTestCase
+
+@end
+
+@implementation Slides3DTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
